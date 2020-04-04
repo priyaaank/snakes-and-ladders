a63eb903c2594733d683593e9ae0771068dd8886
Priyank Gupta <priyaaank@gmail.com>
2020-04-04 12:37:21 +0530

    Added a package and created an empty unit test file for the game

--- src/com/snakesandladders/game/SnakesAndLaddersGame.java
+++ src/com/snakesandladders/game/SnakesAndLaddersGame.java
@@ -1,3 +1,5 @@
+package com.snakesandladders.game;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
---
+++ test/com/snakesandladders/game/SnakesAndLaddersGameTest.java
@@ -0,0 +1,7 @@
+package com.snakesandladders.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnakesAndLaddersGameTest {
+
+}
