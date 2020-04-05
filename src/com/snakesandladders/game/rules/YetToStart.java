769dafc0d19a67cdafb38e1f481dad24f538ed3c
Priyank Gupta <priyaaank@gmail.com>
2020-04-06 00:24:14 +0530

    Distinguishing yet to start player and passing chances for backward compatibility

---
+++ src/com/snakesandladders/game/rules/YetToStart.java
@@ -0,0 +1,11 @@
+package com.snakesandladders.game.rules;
+
+import com.snakesandladders.game.elements.Player;
+import com.snakesandladders.game.state.Turn;
+
+public class HasntRolledSix implements GameRule {
+    @Override
+    public void evaluate(Player player, Turn turn, RuleEvaluationListener listener) {
+
+    }
+}
---
+++ test/com/snakesandladders/game/io/InMemoryLogger.java
@@ -0,0 +1,25 @@
+package com.snakesandladders.game;
+
+import com.snakesandladders.game.io.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryLogger implements Logger {
+
+    private List<String> messageHistory;
+
+    public InMemoryLogger() {
+        this.messageHistory = new ArrayList<>();
+    }
+
+    @Override
+    public void log(String message) {
+        System.out.println("Storing message " + message);
+        this.messageHistory.add(message);
+    }
+
+    public String getMessageAtPosition(Integer index) {
+        return messageHistory.get(index);
+    }
+}
